86b8ce89b395335919125cb2f41e286de905c79a
Brian <brian.fogarty22@gmail.com>
2020-04-20 13:40:42 -0400

    Added more data

--- data.py
+++ data.py
@@ -6,40 +6,49 @@ Created on Thu Apr 16 13:46:55 2020
 """
 
 import pandas
+import getpass
 
 #parameters
 start_day = "1/22/20"
-data_out = pandas.DataFrame(columns = ['Province_State','Admin2','Date','Val'])
+data_out = pandas.DataFrame(columns = ['Province_State','Admin2','Date','Confirmed','Deaths'])
 
-#get data from git
+#get data from git (confirmed)
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 confirmed = pandas.read_csv(url,error_bad_lines=False)
+
+#get data from git (confirmed)
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+deaths = pandas.read_csv(url,error_bad_lines=False)
 
 #column range
 col = confirmed.columns.get_loc(start_day)
 num_cols = len(confirmed.columns)
 
-#testing
-#print('Start Col: ' + str(col))
-#print('Stop Col: ' + str(num_cols))
-
+#loop through all date columns. assumption that all source files have same date columns
 while col < num_cols:
     
     #create the daily dataframe slice
     col_name = confirmed.columns[col]
-    confirmed_day = confirmed[['Province_State','Admin2',col_name]]
-    confirmed_day['Date'] = col_name
-    confirmed_day['Val'] = confirmed_day[col_name]
     
-    #df = confirmed_day[['Province_State','Admin2','Date','Val']]
+    #confirmed cases
+    confirmed_day = confirmed[['UID','Province_State','Admin2',col_name]]
+    confirmed_day['Date'] = col_name
+    confirmed_day['Confirmed'] = confirmed_day[col_name]
+    
+    #deaths
+    deaths_day = deaths[['UID','Province_State','Admin2',col_name]]
+    deaths_day['Date'] = col_name
+    deaths_day['Deaths'] = deaths_day[col_name]
+    
+    day = confirmed_day.merge(deaths_day[['UID','Deaths']], on='UID')
     
     #append the records to the output df
-    data_out = data_out.append(confirmed_day[['Province_State','Admin2','Date','Val']],ignore_index=True)
+    data_out = data_out.append(day[['UID','Province_State','Admin2','Date','Confirmed','Deaths']],ignore_index=True)
     
     #increment day
     col = col + 1
     
 #write the output file
-url = 'C:\\Users\\534507\\Documents\\GitHub\\covid-19\\data\\confirmed.csv'
+url = 'C:\\Users\\' + getpass.getuser() + '\\Documents\\GitHub\\\covid-19\\data\\covid_data.csv'
 data_out.to_csv(url)
 
